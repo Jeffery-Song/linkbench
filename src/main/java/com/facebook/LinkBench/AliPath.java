1cbdf01d114805003f7e3b4479c4286f4c159097
xiaoniu.sxn <xiaoniu.sxn@outlook.com>
2020-10-26 16:42:12 +0800

    minor

--- src/main/java/com/facebook/LinkBench/AliPath.java
+++ src/main/java/com/facebook/LinkBench/AliPath.java
@@ -3,5 +3,6 @@ import java.util.ArrayList;
 
 public class AliPath {
     public static ArrayList<ArrayList<Integer>> paths;
+    // seperate by ","
     public static ArrayList<String> pathstring;
 }
